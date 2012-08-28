46d8fc9db149bbecfb25b673a8b2907acee17928
Arjun Venkataswamy <avenkat2@gmail.com>
2012-08-28 15:47:06 -0500

    changed migrations so user_id was never a string

--- db/migrate/20120828001127_create_sections.rb
+++ db/migrate/20120828001127_create_sections.rb
@@ -1,7 +1,7 @@
 class CreateSections < ActiveRecord::Migration
   def change
     create_table :sections do |t|
-      t.string :user_id
+      t.integer :user_id
       t.string :session_id
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120828013853) do
+ActiveRecord::Schema.define(:version => 20120828002445) do
 
   create_table "enrollments", :force => true do |t|
     t.integer  "user_id"
@@ -21,10 +21,10 @@ ActiveRecord::Schema.define(:version => 20120828013853) do
   end
 
   create_table "sections", :force => true do |t|
-    t.integer  "user_id",    :limit => 255
+    t.integer  "user_id"
     t.string   "session_id"
-    t.datetime "created_at",                :null => false
-    t.datetime "updated_at",                :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
