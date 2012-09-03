d2ea84dbc7373a51b4c1c3715b7d97b97dfa9e06
Farhaan Shaikh <farhaanshaikh19@gmail.com>
2012-09-03 13:27:12 -0500

    respelled prerequisites in model AND changed color scheme

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ActiveRecord::Base
-  attr_accessible :description, :objectives, :prequisites, :title, :user_id
+  attr_accessible :description, :objectives, :prerequisites, :title, :user_id, :image_url
 
   belongs_to :user
   has_many :sections
