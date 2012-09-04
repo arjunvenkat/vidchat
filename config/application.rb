d98f152b61414e3f358fdfecbb9cff3c85d0be6c
Arjun Venkataswamy <avenkat2@gmail.com>
2012-09-03 19:35:01 -0500

    added assets compile line to application.rb

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,8 @@ if defined?(Bundler)
   # Bundler.require(:default, :assets, Rails.env)
 end
 
+config.assets.precompile += %w( *.js *.css )
+
 module Vidchat
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
